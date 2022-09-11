04b8c695d0cdb45cb42908d2ee928267893cb94d
jakubelis <jakub.elis007@gmail.com>
2022-09-11 22:00:57 +0200

    Added sign-up page

--- web/src/app/sign-up/sign-up.component.ts
+++ web/src/app/sign-up/sign-up.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
+  hide = true;
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
+  username = new FormControl('', [Validators.required]);
 
-  constructor() { }
-
-  ngOnInit(): void {
+  getEmailErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
   }
 
+  getPasswordErrorMessage() {
+    if (this.password.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return '';
+  }
+
+  getUsernameErrorMessage() {
+    if (this.username.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return '';
+  }
+  constructor() {}
+
+  ngOnInit(): void {}
 }
